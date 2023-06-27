d3d6e6b86fcc18d329069dbb586d6273cffa0b04
rifaldiarifin <adirifaldiarifin@gmail.com>
2023-06-27 19:34:40 +0700

    Setup ESLint, Prettier, and Husky

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, { Application, Response, Request, NextFunction } from "express";
+import express, { type Application, type Response, type Request } from 'express'
 
-const app: Application = express();
-const port: Number = 4000;
+const app: Application = express()
+const port: number = 4000
 
-app.use("/health", (req: Request, res: Response, next: NextFunction) => {
- res.status(200).send({ statusCode: 200, status: "OK" });
-});
+app.use('/health', (req: Request, res: Response) => {
+  res.status(200).send({ statusCode: 200, status: 'OK' })
+})
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`))
